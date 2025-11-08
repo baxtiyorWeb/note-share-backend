5171e3077325001db5cebe07b215491679bf39ae
baxtiyorWeb <baxtiyorqurbonnazarov33@gmail.com>
2025-11-09 01:41:04 +0500

    fix

--- src/notes/dto/not-update-dto.ts
+++ src/notes/dto/not-update-dto.ts
@@ -1,10 +1,19 @@
-import { IsOptional, IsString } from "class-validator";
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class UpdateNoteDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  content: string;
+
+  @IsOptional()
+  @IsBoolean()
+  is_code_mode?: boolean;
+
   @IsOptional()
   @IsString()
-  title?: string;
-  @IsOptional()
-  @IsString()
-  content?: string;
+  code_language?: string | null;
 }
